cc45975a5de924b2805895d37da1665c4eda9f08
Inge Heeringa <inge.heeringa@gmail.com>
2024-11-26 17:28:01 +0100

    Remove comments

--- app/qui-suis-je/page.tsx
+++ app/qui-suis-je/page.tsx
@@ -14,7 +14,6 @@ const QuiSuisJe = () => {
       <main className="flex flex-col items-center justify-center w-full px-[100px] py-[50px] pb-[200px] gap-[30px]">
         <h2 className="font-bodoni self-start text-3xl">Qui-suis-je ?</h2>
         <div className="grid grid-cols-1 gap-[30px] md:grid-cols-5 md:grid-rows-2 md:gap-[60px]">
-          {/* Image: Default comes first on mobile, grid changes on md */}
           <div className="col-span-1 row-span-1 md:col-span-2 md:row-span-1 flex justify-center items-center h-96 order-1 md:order-1">
             <Image
               className="w-full h-full object-cover"
@@ -24,8 +23,6 @@ const QuiSuisJe = () => {
               height={400}
             />
           </div>
-
-          {/* Text 1 */}
           <p className="font-poppins leading-[3rem] font-light flex items-center text-justify col-span-1 row-span-1 md:col-span-3 md:row-span-1 order-2 md:order-2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
             magna non sem tristique volutpat. Pellentesque venenatis orci eu
@@ -37,13 +34,9 @@ const QuiSuisJe = () => {
             Pellentesque venenatis orci eu nulla molestie, sit amet aliquet urna
             varius.
           </p>
-
-          {/* Quote */}
           <span className="font-bodoni text-4xl flex items-center text-center md:text-right col-span-1 md:col-span-2 row-span-1 order-3 md:order-4">
             &quot;Suspendisse id pharetra leo, quis mollis lorem.&quot;
           </span>
-
-          {/* Text 2 */}
           <p className="font-poppins leading-[3rem] font-light flex items-center text-justify col-span-1 row-span-1 md:col-span-3 md:row-span-1 md:col-start-1 md:row-start-2 order-4 md:order-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
             magna non sem tristique volutpat. Pellentesque venenatis orci eu
