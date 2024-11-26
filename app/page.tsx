87577a90a43f223c82da88d02fb0be5c43962e03
Gaston82 <cabezaartaza@hotmail.com>
2024-11-26 20:25:06 +0100

    add hero imege

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+import heroImage from "@/public/stock/photo4.jpg";
+
 export default function Home() {
-  return <h1 className="font-bodoni text-5xl my-11">Domikrea</h1>;
+  return (
+    <>
+      <section>
+        <Image
+          src={heroImage}
+          alt="Productos de costura"
+          sizes="100vw"
+          // Make the image display full width
+          style={{
+            width: "100%",
+            height: "400px",
+            objectFit: "cover",
+          }}
+        />
+      </section>
+      <main className="flex flex-col align-middle justify-center gap-20 pt-24 pr-[300] pb-48 pl-24">
+        <div className="flex flex-col justify-center align-middle text-center">
+          <span className="text-5xl font-bodoni">Bienvenue !</span>
+          <p className="font-poppins leading-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
+            magna non sem tristique volutpat. Pellentesque venenatis orci eu
+            nulla molestie, sit amet aliquet urna varius. Suspendisse id
+            pharetra leo, quis mollis lorem.
+          </p>
+        </div>
+      </main>
+    </>
+  );
 }
