af62ce31c716e6b6477d34a3f57a1499a21a9068
Inge Heeringa <inge.heeringa@gmail.com>
2025-01-14 19:00:15 +0100

    Add divider to header bottom

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import Image from "next/image";
-import logo from "@/public/domikrea-logo.jpg";
-import Navbar from "./Navbar";
+import React from 'react';
+import Image from 'next/image';
+import logo from '@/public/domikrea-logo.jpg';
+import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center px-14">
-      <Image src={logo} alt="Domikrea logo" width="236" height="126" />
-      <Navbar />
-    </header>
+    <>
+      <header className="flex justify-between items-center px-14">
+        <Image src={logo} alt="Domikrea logo" width="236" height="126" />
+        <Navbar />
+      </header>
+      <div className="w-full h-[1px] bg-gray-300"></div>
+    </>
   );
 };
 
