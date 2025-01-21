0ff3c34c512d0dbc4f123a56b55d69ac685b8531
Gaston82 <cabezaartaza@hotmail.com>
2025-01-21 14:55:37 +0100

    Add Button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,25 @@
+import { Bodoni_Moda } from 'next/font/google';
+
+const bodoniModa = Bodoni_Moda({ weight: '400', subsets: ['latin'] });
 interface ButtonProps {
   element: 'anchor' | 'button';
   text: string;
   href?: string;
 }
-
 const Button = ({ element, text, href }: ButtonProps): JSX.Element =>
   element === 'button' ? (
-    <button className="flex items-center justify-center text-white bg-primary border border-primary px-10 py-2.5 rounded-md text-base transition duration-200 hover:text-primary hover:bg-transparent">
+    <button
+      className={`${bodoniModa.className} flex items-center justify-center text-white bg-primary border border-primary px-10 py-2.5 rounded-md text-base transition duration-200 hover:text-primary hover:bg-transparent`}
+    >
       {text}
     </button>
   ) : (
-    <a href={href}>{text}</a>
+    <a
+      className={`${bodoniModa.className} flex items-center justify-center text-white bg-primary border border-primary px-10 py-2.5 rounded-md text-base transition duration-200 hover:text-primary hover:bg-transparent`}
+      href={href}
+    >
+      {text}
+    </a>
   );
 
 export default Button;
