08ad69ee38df9886204c927a7865152aac39b1ba
Gaston82 <cabezaartaza@hotmail.com>
2025-01-21 10:51:00 +0100

    Install fortawesome

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="font-bodoni text-5xl my-11">Domikrea</h1>;
+  return <h1 className="font-bodoni text-5xl my-11">Domikrea prueba home</h1>;
 }
